6ec61bfca0b1db186ded289199fff20bd7d7c639
email <robinsondepot@gmail.com>
2022-04-14 13:33:24 +0000

    ma partie index

---
+++ index.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Menu</title>
+    <?php include 'pages/style.php'?>
+</head>
+<body>    
+<?php include 'pages/menu.php'?>
+
+<div class="col md-12 d-flex"> 
+<div class="col md-7 "> 
+<div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
+  <div class="carousel-indicators">
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+  </div>
+  <div class="carousel-inner container-fluid">
+    <div class="carousel-item active">
+      <img src="image/img3.jpg" class="d-block w-100 " height="555px" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="image/img2.jpg" class="d-block w-100 " height="555px" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="image/img6.jpg" class="d-block w-100" height="555px" alt="...">
+    </div>
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
+</div>
+<div class="col md-5" style="background-color:yellow;"> <br><br><br>
+    <h1 class="text-center " style="font-family: 'Lucida Calligraphy;font-size:2em;">Soyez la bienvenue à la gestion des patients<br> DELBENIE <br> Hospitalité_Bien-être_Santé </h1>
+</div>
+</div>
+<br>
+<?php include 'pages/footer.php'?>
+<?php include 'pages/js.php'?>
+</body>
+</html>
--- pages/js.php
+++ pages/js.php
@@ -1,4 +1,4 @@
-<script src="bootstrap-5.1.3-dist/js/bootstrap.js"></script>
+      <script src="bootstrap-5.1.3-dist/js/bootstrap.js"></script>
       <script src="bootstrap-5.1.3-dist/js/bootstrap.min.js"></script>
       <script src="bootstrap-5.1.3-dist/js/main.js"></script>
       <script src="bootstrap-5.1.3-dist/js/jquery-3.3.1.slim.min.js"></script>
