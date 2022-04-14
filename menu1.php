4d8f29238f6e96ff08ca4f09847facd529fa895e
email <robinsondepot@gmail.com>
2022-04-14 09:37:55 +0000

    ma partie menu

---
+++ menu1.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Menu1</title>
+    
+</head>
+<body>
+    
+</body>
+</html>
---
+++ pages/footer.php
@@ -0,0 +1,5 @@
+<div class="pied bg-primary text-light text-center">
+<footer>
+  <h5 class="p-3">Copyright DELBENIE 2022</h5>
+</footer>
+</div>
