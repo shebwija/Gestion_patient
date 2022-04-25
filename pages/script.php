58a7ebd711e73b82ecffcf5dad26e37539337824
email <robinsondepot@gmail.com>
2022-04-25 10:14:20 +0000

    scrip

--- pages/script.php
+++ pages/script.php
@@ -1,2 +1,7 @@
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" 
-integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+
+      <script src="bootstrap-5.1.3-dist/js/bootstrap.js"></script>
+      <script src="bootstrap-5.1.3-dist/js/bootstrap.min.js"></script>
+      <script src="bootstrap-5.1.3-dist/js/main.js"></script>
+      <script src="bootstrap-5.1.3-dist/js/jquery-3.3.1.slim.min.js"></script>
+      <script src="bootstrap-5.1.3-dist/js/bootstrap.bundle.js"></script>
+      <script src="bootstrap-5.1.3-dist/js/popper.min.js"></script>
